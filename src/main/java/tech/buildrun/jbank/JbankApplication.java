4e225fee19af3bc33ba91d6f05aedd9d25923ea2
paulohgd <paulohguerra@outlook.com>
2025-01-07 11:43:08 -0300

    First commit

---
+++ src/main/java/tech/buildrun/jbank/JbankApplication.java
@@ -0,0 +1,13 @@
+package tech.buildrun.jbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/buildrun/jbank/JbankApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.buildrun.jbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
